237b5e1d5a84a1fe81d0d96748bf83665a4a6a9a
Gaddour <mannai.abdlkader@gmail.com>
2024-01-13 03:19:58 +0100

    Restyled habit View, cleaned some code and files, and some othe styling tweaks

--- assets/scripts/habit-view.js
+++ assets/scripts/habit-view.js
@@ -1,167 +1,36 @@
-var habit1 = Habit(
-  "Exercise",
-  "Fitness",
-  "Workout for 30 minutes daily",
-  7,
-  HabitsID()
-);
-var habit2 = Habit(
-  "Read",
-  "Self-Improvement",
-  "Read a book for 20 minutes daily",
-  5,
-  HabitsID(),
-  [
-    { note: "Finished a chapter", rate: 4, done: true },
-    { note: "Started a new book", rate: 5, done: true },
-  ]
-);
-var habit3 = Habit(
-  "Meditate",
-  "Mindfulness",
-  "Practice meditation for 15 minutes daily",
-  10,
-  HabitsID()
-);
-var habit4 = Habit(
-  "Learn a Language",
-  "Skill Development",
-  "Study a new language for 30 minutes daily",
-  7,
-  HabitsID(),
-  [
-    { note: "learned alphapet", rate: 5, done: true },
-    { note: "lerned numbers ", rate: 4, done: true },
-  ]
-);
-var habit5 = Habit(
-  "Drink Water",
-  "Health",
-  "Drink 8 glasses of water daily",
-  15,
-  HabitsID(),
-  [
-    { note: "drunk 2 L of water", rate: 5, done: true },
-    { note: "it 's too cold  ", rate: 1, done: false },
-  ]
-);
-var habit6 = Habit(
-  "Write",
-  "Creativity",
-  "Write for 20 minutes daily",
-  5,
-  HabitsID(),
-  [
-    { note: "wrote 2 pages about my day", rate: 5, done: true },
-    { note: " wasn't feeling inspired", rate: 1, done: false },
-  ]
-);
-var habit7 = Habit(
-  "Eat Vegetables",
-  "Health",
-  "Include vegetables in every meal",
-  10,
-  HabitsID()
-);
-var habit8 = Habit(
-  "Practice Guitar",
-  "Hobby",
-  "Play the guitar for 20 minutes daily",
-  7,
-  HabitsID()
-);
-var habit9 = Habit(
-  "Code",
-  "Skill Development",
-  "Code for 1 hour daily",
-  7,
-  HabitsID()
-);
-var habit10 = Habit(
-  "Practice Yoga",
-  "Fitness",
-  "Practice yoga for 30 minutes daily",
-  10,
-  HabitsID(),
-  []
-);
-var habit11 = Habit(
-  "Limit Screen Time",
-  "Health",
-  "Limit screen time to 2 hours daily",
-  15,
-  HabitsID()
-);
-var habit12 = Habit(
-  "Review Goals",
-  "Self-Improvement",
-  "Review personal and professional goals daily",
-  7,
-  HabitsID()
-);
-var habit13 = Habit(
-  "Quality Sleep",
-  "Health",
-  "Get 7-8 hours of quality sleep",
-  15,
-  HabitsID()
-);
-var habit14 = Habit(
-  "Draw",
-  "Creativity",
-  "Draw or sketch for 20 minutes daily",
-  5,
-  HabitsID()
-);
-var habit15 = Habit(
-  "Express Gratitude",
-  "Mindfulness",
-  "Write down three things to be grateful for daily",
-  10,
-  HabitsID()
-);
-
-habit1.progress = habit1.progress.concat([
-  { note: "Feeling energized!", rate: 5, done: true },
-  { note: "Missed today", rate: 2, done: false },
-  { note: "Feeling energized!", rate: 5, done: true },
-  { note: "Done it", rate: 3, done: true },
-]);
-
-var habits = [
-  habit1,
-  habit2,
-  habit3,
-  habit4,
-  habit5,
-  habit6,
-  habit7,
-  habit8,
-  habit9,
-  habit10,
-  habit11,
-  habit12,
-  habit13,
-  habit14,
-  habit15,
-];
+// console.log(getURLHabit());
 
 var currentHabit = findObject(storedHabits, "id", Number(getURLHabit()))[0];
 
-console.log(getURLHabit());
-$(".card-title").append(`${currentHabit.name}`);
-$(".habit-category").append(`${currentHabit.category}`);
-$(".habit-description").append(`${currentHabit.description}`);
-$(".habit-achivedd").append(`${currentHabit.goalAcheived}`);
-$(".habit-achived").append(`${currentHabit.goalAcheived}`);
-$(".habit-avg-reating").append(`${avgrating(currentHabit.progress)}`);
-each(currentHabit.progress, function (element) {
-  if (element.done === true) {
-    $(".habitcol-progress").append(`<div class="daily-state done"></div>`);
-  } else {
-    $(".habitcol-progress").append(`<div class="daily-state not-done"></div>`);
+function displayHabit(habit) {
+  $("#habit-name").text(habit.name);
+  $("#habit-category").text(habit.category);
+  $("#habit-description").text(habit.description);
+  $("#goal").text(habit.goal);
+  $("#streak").text(habit.streaks);
+
+  progress = (100 * habit.streaks) / habit.goal;
+  $("#green-ratio").css("flex", "0 0 " + progress + "%");
+  $("#red-ratio").css("flex", "0 0 " + (100 - progress) + "%");
+
+  if (progress === 0 || progress === 100) {
+    $("#green-ratio.ratio").css("border-radius", "15px 15px 15px 15px");
+    $("#red-ratio.ratio").css("border-radius", "15px 15px 15px 15px");
   }
-});
+
+  $achieved = $("#achieved");
+}
+
+displayHabit(currentHabit);
+
+// each(currentHabit.progress, function (element) {
+//   if (element.done === true) {
+//     $(".habitcol-progress").append(`<div class="daily-state done"></div>`);
+//   } else {
+//     $(".habitcol-progress").append(`<div class="daily-state not-done"></div>`);
+//   }
+// });
+
 var $note = $("#habit-note");
 var $;
 var $done = $("input[name='habit-done']");
